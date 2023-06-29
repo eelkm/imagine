2b57a42cb1b8a8c870771c9aff2c68a0c8056fb3
eelkm <edgarsapinis1@gmail.com>
2023-06-29 19:54:27 +0300

    Changed header

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,21 @@ function App() {
     <BrowserRouter>
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to='/'>
-          <img src={logo} alt='logo' className='w-28 object-contain' />
+          <div className='flex'>
+            <img src={logo} alt='logo' className='w-10 object-contain' />
+            <div className='p-1 pl-2 text-2xl font-bold'>
+              Imagine<span className='text-xs font-extralight'>.edgarsapinis.dev</span>
+            </div>
+          </div>
+
         </Link>
 
-        <Link to={'/create-post'} className='font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
+        <Link to={'/create-post'} className='glow font-medium bg-[#000000] text-white px-4 py-2 rounded-md'>
           Create
         </Link>
       </header>
 
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#ffffff] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/create-post' element={<CreatePost />} />
